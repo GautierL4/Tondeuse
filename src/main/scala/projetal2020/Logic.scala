322fd9391900af22fe0e711f5a89ec07816313d5
GautierL <gautier.laurent4@gmail.com>
2021-01-20 20:51:17 +0100

    start defining case class / trait

--- src/main/scala/projetal2020/Logic.scala
+++ src/main/scala/projetal2020/Logic.scala
@@ -1,3 +1,27 @@
 package projetal2020
 
 case class Point(x: Int, y: Int)
+
+case class State(position: Point, direction: Char)
+
+trait Action {
+  def apply(code: Char): Unit
+}
+
+class MoveRight extends Action {
+  override def apply(code: Char): Unit = {
+    println(code)
+  }
+}
+
+class MoveLeft extends Action {
+  override def apply(code: Char): Unit = {
+    println(code)
+  }
+}
+
+class MoveFoward extends Action {
+  override def apply(code: Char): Unit = {
+    println(code)
+  }
+}
--- src/main/scala/projetal2020/Main.scala
+++ src/main/scala/projetal2020/Main.scala
@@ -3,6 +3,7 @@ package projetal2020
 object Main extends App {
   println("Ici le programme principal")
   val point = Point(1, 2)
+  val state = State(point, 'A')
   // Le code suivant ne compilera pas.
   // var tmp = null;
   // var tmp2 = if (tmp == 1) "yes" else 1
