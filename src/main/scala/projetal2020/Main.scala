d6e39ff0a04786c43ea95a9f5433a67108edf665
enes uyar <enes.uyar@pm.me>
2021-01-23 21:13:03 +0100

    added input data from file

--- src/main/scala/projetal2020/Main.scala
+++ src/main/scala/projetal2020/Main.scala
@@ -1,12 +1,38 @@
 package projetal2020
 
+import scala.io.Source
+
 object Main extends App {
-  println("Ici le programme principal")
-  val point = Point(1, 2)
-  val state = State(point, "N")
+
+  val data_from_file = Source
+    .fromFile("/home/azote/Documents/projets_esgi/input_example.txt")
+    .getLines()
+    .toList
+
+  // 5 5 ==> dimension du terrain
+  val xmax: Int = data_from_file(0).split(" ")(0).toInt
+  val ymax: Int = data_from_file(0).split(" ")(1).toInt
+  val xmin: Int = 0
+  val ymin: Int = 0
+  println(s"Taille du terrain: ${xmax.toString} x ${ymax.toString}")
+
+  // 1 2 N ==> orientation
+  val orientation_x: Int = data_from_file(1).split(" ")(0).toInt // récup 1
+  val orientation_y: Int = data_from_file(1).split(" ")(1).toInt // récup 2
+  val orientation_NEWS: String = data_from_file(1).split(" ")(2) // récup "N"
+
+  println(
+    s"Orientation: ${orientation_x.toString} , ${orientation_y.toString} , ${orientation_NEWS}"
+  )
+  val point = Point(orientation_x, orientation_y)
+  val state = State(point, orientation_NEWS)
+
+  // GAGAGAGAA ==> instructions
+  //data_from_file(2).toCharArray
   val actionFoward = MoveFoward("A")
   val actionRight = MoveRight("D")
   val actionLeft = MoveLeft("G")
+
   val instructions: List[Action] =
     List[Action](
       actionFoward,
@@ -15,14 +41,11 @@ object Main extends App {
       actionRight,
       actionLeft
     )
+
   val tondeuse = new Tondeuse(state, instructions)
   val newState: State = tondeuse.computeInstructions()
   println(newState.direction)
   println(newState.position.x)
   println(newState.position.y)
-  // Le code suivant ne compilera pas.
-  // var tmp = null;
-  // var tmp2 = if (tmp == 1) "yes" else 1
 
-  // println(s"tmp: $tmp, tmp2: $tmp2")
 }
